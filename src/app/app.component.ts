f8cf6ca334b22443802f38fea25684b8236c99a4
SL <abletory@gmail.com>
2021-05-17 21:04:01 +0300

    - check filetype;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,23 +21,28 @@ export class AppComponent {
     let fileReader: FileReader = new FileReader();
     if (fileList.length) {
 
-    fileReader.onloadend = () => {
+      fileReader.onloadend = () => {
 
-      let str = JSON.parse(JSON.stringify(fileReader.result));
-      str = str.replace(/\r\n/g, '');
-      str = str.split(' ').join('');
+        let str = JSON.parse(JSON.stringify(fileReader.result));
+        str = str.replace(/\r\n/g, '');
+        str = str.split(' ').join('');
 
-      let arr = JSON.stringify(eval(str));
-      this.dataService.chartDataLoaded(JSON.parse(arr));
+        try {
+          let arr = JSON.stringify(eval(str));
+          this.dataService.chartDataLoaded(JSON.parse(arr));
+        }
+        catch (error) {
+          alert('.json file is corrupted');
+          let arr = [];
+          this.dataService.chartDataLoaded(arr);
+        }
+      };
 
-    };
-
-    fileReader.readAsText(file);
-  }
-  else
-  {
+      fileReader.readAsText(file);
+    }
+    else {
       let arr = [];
       this.dataService.chartDataLoaded(arr);
-  }
+    }
   }
 }
